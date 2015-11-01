c10fca275432ebe2305aacdbf0439fb277382b47
mwinter80 <mwinter@unboundedpress.org>
2015-11-01 14:56:10 -0800

    2015 fundraiser

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ from datetime import *
 from django.template.loader import render_to_string
 
 def home(request, name):
-    fundraiser = Event.objects.get(name='the wulf. 2014 fundraiser')
+    fundraiser = Event.objects.get(name='the wulf. 2015 fundraiser')
     upcoming_event_list = Event.objects.filter(start_date__gte=datetime.now()).order_by('start_date')
     venue = AbstractProfile.objects.select_subclasses().get(name='the wulf.')
     return render_to_response('home.html', {'name': name, 'venue': venue, 'upcoming_event_list': upcoming_event_list, 'fundraiser': fundraiser})
@@ -15,14 +15,14 @@ def events(request, name):
     return events_by_year(request, name, '2015')
 
 def events_by_year(request, name, year):
-    fundraiser = Event.objects.get(name='the wulf. 2014 fundraiser')
+    fundraiser = Event.objects.get(name='the wulf. 2015 fundraiser')
     upcoming_event_list = Event.objects.filter(start_date__gte=datetime.now()).order_by('start_date')
     past_event_list = Event.objects.filter(start_date__year=year, start_date__lt=datetime.now()).order_by('-start_date')
     #past_event_list = Event.objects.filter(name = 'Experimental Music Yearbook')
     return render_to_response('events.html', {'name': name, 'upcoming_event_list':upcoming_event_list, 'past_event_list':past_event_list, 'fundraiser': fundraiser})
 
 def donate(request, name):
-    fundraiser_event = Event.objects.get(name = 'the wulf. 2014 fundraiser')
+    fundraiser_event = Event.objects.get(name = 'the wulf. 2015 fundraiser')
     return render_to_response('donate.html', {'name': name, 'fundraiser_event': fundraiser_event})
 
 def membership(request, name):
