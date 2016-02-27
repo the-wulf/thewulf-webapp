c7870786220235d28b4dabd5b0d717b5033f60ce
mwinter80 <mwinter@unboundedpress.org>
2016-02-27 15:27:27 -0800

    making individual event views

--- views.py
+++ views.py
@@ -20,6 +20,10 @@ def events_by_year(request, name, year):
     past_event_list = Event.objects.filter(start_date__year=year, start_date__lt=datetime.now()).order_by('-start_date')
     #past_event_list = Event.objects.filter(name = 'Experimental Music Yearbook')
     return render_to_response('events.html', {'name': name, 'upcoming_event_list':upcoming_event_list, 'past_event_list':past_event_list, 'fundraiser': fundraiser})
+    
+def event_detail(request, id):
+    event = Event.objects.get(id = id)
+    return render_to_response('event_module.html', {'event': event, 'verbose': True})
 
 def donate(request, name):
     fundraiser_event = Event.objects.get(name = 'the wulf. 2015 fundraiser')
