26ebf20b967e641942c393481c91420475ea1955
mwinter80 <mwinter@unboundedpress.org>
2016-02-27 16:39:43 -0800

    making title clickable

--- views.py
+++ views.py
@@ -23,7 +23,7 @@ def events_by_year(request, name, year):
     
 def event_details(request, name,  id):
     event = Event.objects.get(id = id)
-    return render_to_response('event_details.html', {'event': event, 'verbose': True})
+    return render_to_response('event_details.html', {'event': event, 'verbose': 'true'})
 
 def donate(request, name):
     fundraiser_event = Event.objects.get(name = 'the wulf. 2015 fundraiser')
